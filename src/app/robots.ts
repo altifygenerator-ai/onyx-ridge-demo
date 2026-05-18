4ca81384b444e249851cb15718cbd1a835b748f3
AltifyDev <altifygenerator@gmail.com>
2026-05-17 19:57:45 -0500

    robots

---
+++ src/app/robots.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "Facebot",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://onyxridge.net/sitemap.xml",
+  };
+}
